5837ba7f2a090458657c41f5d81163a51f64427e
Vlad <vladdp70@gmail.com>
2021-06-14 19:45:39 +0300

    Пример использования значений функц на промисах

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 "use strict";
 
-console.log("start"); //1
+const number = 42;
 
-setTimeout(() => {
-  console.log("timer");//7
-}, 0);
-
-const p1 = new Promise((resolve, reject) => {
-  console.log("create prom"); //2
-  resolve();
-  reject();
-  console.log("prom created"); //3
-});
-
-p1.then(() => {
-  console.log("prom cb body"); //5
-})
-  .catch(() => {
-    console.log("catch body");//5 если ошибка
-  })
-  .finally(() => {
-    console.log("fin");//6
+const numberPromise = Promise.resolve(number);
+function handlePromise(promise) {
+  return promise.then((data) => {
+    console.log("Data is:", data);
   });
-
-console.log("end"); //4
+}
+handlePromise(numberPromise);
