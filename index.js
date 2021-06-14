67af6d8232ea991dbc472086254297cc7d1e9cae
Vlad <vladdp70@gmail.com>
2021-06-14 19:05:09 +0300

    Кастомная ошибка

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ fetch("./data.json")
   })
   .catch((error) => {
     console.log("Error");
-    if(error instanceof TypeError){
+    if (error instanceof TypeError) {
       console.log(10);
     }
 
-    if(error instanceof SyntaxError){
+    if (error instanceof SyntaxError) {
       console.log(1);
     }
-   
+
     document.body.append("Error happened");
   })
   .finally(() => {
     document.getElementById("load").remove();
-  })
+  });
+
+  class UserError extends Error {// совои ошибки кастомные
+    constructor(str) {
+      super(str);
+      this.userType = 'user';
+      this.name = 'User Error'
+      Error.captureStackTrace(this, UserError);
+    }
+  }
